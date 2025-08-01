bccc7e2b30a8f2f8b81eb34e8633a4628ae19704
Chris <32988106+Noobot9k@users.noreply.github.com>
2025-07-31 19:21:19 -0600

    working loop effect on web

--- GodotProject/Player/LoopControl.gd
+++ GodotProject/Player/LoopControl.gd
@@ -3,9 +3,11 @@ class_name LoopControl
 
 @export var LoopSize : float = 24
 @export var RemotePositions : Array[Node3D]
+@export var LoopRenderSurface : MeshInstance3D
 @export var PlayerChar : PlayerCharacter
 
 func _process(_delta: float) -> void:
 	PlayerChar.voidout_hight = -LoopSize/2
+	LoopRenderSurface.position.y = -LoopSize/2
 	for remotePos in RemotePositions:
 		remotePos.position.y = LoopSize
